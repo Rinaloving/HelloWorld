5c6a31c78f6dae07063e06fc817aa455c1e7b0cf
Rina <cfl1989@live.com>
2018-04-11 20:24:05 +0800

    winform实现选中图片预览

---
+++ TestListView/TestListView/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace TestListView
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listView1.LabelWrap = false;
+            this.listView1.Location = new System.Drawing.Point(0, 254);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(514, 97);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.List;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(427, 30);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 32);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "选取";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(60, 60);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(408, 254);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 351);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "显示图片";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ TestListView/TestListView/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestListView
+{
+    public partial class Form1 : Form
+    {
+        private string[] files;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Multiselect = true;
+            DialogResult dr = this.openFileDialog1.ShowDialog();
+
+            if (dr == DialogResult.OK)
+            {
+               
+                listView1.Items.Clear();//每次点击事件后将ListView中的数据清空，重新显示
+                imageList1.Images.Clear(); //每次点击事件后将imageList中的图片清空，重新显示
+                files = this.openFileDialog1.FileNames;
+                this.listView1.View = View.LargeIcon;
+                
+                this.listView1.BeginUpdate();
+                for (int i = 0; i < files.Length; i++)
+                {
+                     
+                   
+                    this.imageList1.Images.Add(Image.FromFile(this.files[i]));
+                  
+                    this.listView1.Items.Add(this.files[i].Substring(this.files[i].LastIndexOf(@"\") + 1), i);
+                    this.listView1.LargeImageList = this.imageList1;
+                    pictureBox1.Image = Image.FromFile(openFileDialog1.FileName); //显示第一张
+                    //pictureBox1.Image = Image.FromFile(files[i]); //显示最后一张
+                   
+                   
+                }
+                this.listView1.EndUpdate();
+            }
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (this.listView1.SelectedItems.Count > 0)
+            {
+                this.pictureBox1.ImageLocation = this.files[this.listView1.SelectedItems[0].Index];
+             
+            }
+                
+        }
+
+       
+    }
+}
