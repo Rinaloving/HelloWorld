8818e96d4f925bd72c9893450209449e610fa87f
cfl <1243455143@qq.com>
2018-04-24 14:59:14 +0800

    从PDF中获取图片，可以获取png,jepg等

---
+++ GetPngFromPDF/GetPngFromPDF/Program.cs
@@ -0,0 +1,87 @@
+﻿using O2S.Components.PDFRender4NET;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetPngFromPDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //获取pdf中图片的页数
+            string path = @"C:\Users\Administrator\Desktop\S18\"; // 文件夹路径
+
+            string[] pdfs = Directory.GetFiles(path, "*.pdf"); //获取文件夹下的所有pdf文件
+
+
+
+            foreach (string page in pdfs)
+            {
+                PDFFile pdfFile = PDFFile.Open(page);
+                PDFTranImgHelp.ConvertPDF2Image(page, "C:\\", "NImage", 1, pdfFile.PageCount, ImageFormat.Jpeg, Definition.Five);
+            }     
+                
+
+
+           
+
+
+        }
+    }
+    public enum Definition
+    {
+        One = 1, Two = 2, Three = 3, Four = 4, Five = 5, Six = 6, Seven = 7, Eight = 8, Nine = 9, Ten = 10
+    }
+
+    public class PDFTranImgHelp
+    {
+        /// <summary>
+        /// 将PDF文档转换为图片的方法
+        /// </summary>
+        /// <param name="pdfInputPath">PDF文件路径</param>
+        /// <param name="imageOutputPath">图片输出路径</param>
+        /// <param name="imageName">生成图片的名字</param>
+        /// <param name="startPageNum">从PDF文档的第几页开始转换</param>
+        /// <param name="endPageNum">从PDF文档的第几页开始停止转换</param>
+        /// <param name="imageFormat">设置所需图片格式</param>
+        /// <param name="definition">设置图片的清晰度，数字越大越清晰</param>
+        public static void ConvertPDF2Image(string pdfInputPath, string imageOutputPath,
+            string imageName, int startPageNum, int endPageNum, ImageFormat imageFormat, Definition definition)
+        {
+            PDFFile pdfFile = PDFFile.Open(pdfInputPath);
+            if (!Directory.Exists(imageOutputPath))
+            {
+                Directory.CreateDirectory(imageOutputPath);
+            }
+            // validate pageNum
+            if (startPageNum <= 0)
+            {
+                startPageNum = 1;
+            }
+            if (endPageNum > pdfFile.PageCount)
+            {
+                endPageNum = pdfFile.PageCount;
+            }
+            if (startPageNum > endPageNum)
+            {
+                int tempPageNum = startPageNum;
+                startPageNum = endPageNum;
+                endPageNum = startPageNum;
+            }
+            // start to convert each page
+            for (int i = startPageNum; i <= endPageNum; i++)
+            {
+                Bitmap pageImage = pdfFile.GetPageImage(i - 1, 56 * (int)definition);
+                pageImage.Save(imageOutputPath + imageName + i.ToString() + "." + imageFormat.ToString(), imageFormat);
+                pageImage.Dispose();
+            }
+            pdfFile.Dispose();
+        }
+    }
+}
